49a6338c9e5280be3aca25d2a5d6661bcccaca50
Henry <henrysoverholt@gmail.com>
2019-09-09 13:19:50 -0400

    added form to call api

--- src/app/new-entry/new-entry.component.ts
+++ src/app/new-entry/new-entry.component.ts
@@ -10,25 +10,51 @@ import { NgForm } from "@angular/forms";
 export class NewEntryComponent implements OnInit {
   weatherData: any;
   trafficData: any;
+  trafficString: any;
+  showNewEntry: boolean = false;
+  showTodo: boolean = false;
+  showEvent: boolean = false;
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit() {}
 
+  handleNewEntry(): void {
+    this.showNewEntry = !this.showNewEntry;
+  }
+  handleTypeForm(form: NgForm): void {
+    this.showNewEntry = !this.showNewEntry;
+
+    if (form.value.entryType === "To-do") {
+      this.showTodo = !this.showTodo;
+    } else if (form.value.entryType === "Event") {
+      this.showEvent = !this.showEvent;
+    }
+  }
+  postTodo(todoForm: NgForm): void {
+    console.log(todoForm.value);
+    this.showTodo = false;
+  }
+  handleEventForm() {
+    console.log("form was requested");
+  }
+
   getWeather(eventForm: NgForm): void {
+    console.log(eventForm.value.date, eventForm.value.time);
     this.weatherService
-      .getWeatherData(eventForm.value.zip)
+      .getWeatherData(eventForm.value.eventZip)
       .subscribe(response => {
-        this.weatherData = response.weather[0].icon;
+        this.weatherData = response;
         console.log(this.weatherData);
       });
   }
-  getTraffic(trafficForm: NgForm): void {
-    this.weatherService
-      .getTrafficData(trafficForm.value)
-      .subscribe(response => {
-        this.trafficData = response;
-        console.log(this.trafficData);
-      });
+  getTraffic(eventForm: NgForm): void {
+    console.log(eventForm.value.time, eventForm.value.date);
+    // this.trafficString = trafficForm.value;
+    // this.trafficString.replace(/\s/g, "+");
+    this.weatherService.getTrafficData(eventForm.value).subscribe(response => {
+      this.trafficData = response.rows[0].elements[0];
+      console.log(this.trafficData);
+    });
   }
 }
 // response.weather[0].icon
