e79e3aa3dfc2b51a219d62f93b4f8976717f7a0b
Henry Overholt <52422363+Henry-Overholt@users.noreply.github.com>
2019-09-09 11:58:45 -0400

    Update weather.service.ts

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -17,7 +17,7 @@ export class WeatherService {
   }
   getTrafficData(trafficData: any): Observable<any> {
     return this.http.get(
-      `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${trafficData.startNumber}+${trafficData.startStreet}+${trafficData.startCity}+${trafficData.startState}&destinations=${trafficData.endNumber}+${trafficData.endStreet}+${trafficData.endCity}+${trafficData.endState}&key=AIzaSyCSreLuncbla65v6Bn2bOIMi6ajS4-18Os`
+      `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${trafficData.startNumber}+${trafficData.startStreet}+${trafficData.startCity}+${trafficData.startState}&destinations=${trafficData.endNumber}+${trafficData.endStreet}+${trafficData.endCity}+${trafficData.endState}&key=`
     );
   }
 }
