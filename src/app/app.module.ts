9daa57108c60bb4ff8a44ec9fbdc983dbdd93a3b
Henry <henrysoverholt@gmail.com>
2019-09-10 16:26:47 -0400

    added routes converting function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,34 @@ import { FormsModule } from "@angular/forms";
 import { WeatherService } from "./services/weather.service";
 import { TodoService } from "./services/todo.service";
 import { EventService } from "./services/event.service";
-// import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { NewEntryComponent } from "./new-entry/new-entry.component";
-import { HomeComponent } from './home/home.component';
-import { TodayComponent } from './today/today.component';
-import { TomorrowComponent } from './tomorrow/tomorrow.component';
-import { UpcomingComponent } from './upcoming/upcoming.component';
+import { TodayComponent } from "./today/today.component";
+import { TomorrowComponent } from "./tomorrow/tomorrow.component";
+import { UpcomingComponent } from "./upcoming/upcoming.component";
+import { HomeComponent } from "./home/home.component";
+const appRoutes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "**", component: HomeComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" }
+];
 
 @NgModule({
-  declarations: [AppComponent, NewEntryComponent, HomeComponent, TodayComponent, TomorrowComponent, UpcomingComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    NewEntryComponent,
+    TodayComponent,
+    TomorrowComponent,
+    UpcomingComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [WeatherService, TodoService, EventService],
   bootstrap: [AppComponent]
 })
