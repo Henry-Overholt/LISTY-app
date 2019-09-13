d07f78fd3983824b1059d4cee7a26bc3cfd9a770
Henry <henrysoverholt@gmail.com>
2019-09-13 14:07:52 -0400

    fixed bug, only opens where called, hopefully

--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -16,9 +16,14 @@ export class TodayComponent implements OnInit {
   weatherData: any;
   currentTemp: any;
   trafficData: any;
-  show: boolean = false;
+  show: boolean;
   completedTodo: any[];
-  completed: boolean = false;
+  completed: boolean;
+  currentWeatherData: any;
+  todayReturnDescription: any;
+  todayTemp: any;
+  call: boolean;
+
   constructor(
     private todoService: TodoService,
     private eventService: EventService,
@@ -33,6 +38,14 @@ export class TodayComponent implements OnInit {
     this.eventService
       .getEvent(this.todoService.getDate(), false)
       .subscribe(response => (this.todayEventList = response));
+    this.currentWeather("48226");
+    this.todayTodoList.forEach(todo => {
+      todo.completed = false;
+    });
+    this.todayEventList.forEach(event => {
+      event.call = false;
+      event.show = false;
+    });
   }
   // moveCompleted(i: number): any[] {
   //   this.completedTodo.splice(i);
@@ -45,14 +58,23 @@ export class TodayComponent implements OnInit {
       .deleteTodo(id, this.todoService.getDate(), false)
       .subscribe(response => (this.todayTodoList = response));
   }
-  callApi(event): void {
-    this.getWeather(event.event_zip);
-    this.getTraffic(event);
-    console.log(event);
+  callApi(i): void {
+    this.getWeather(this.todayEventList[i].event_zip);
+    this.todayEventList[i].call = !this.todayEventList[i].call;
+    this.getTraffic(this.todayEventList[i]);
+    console.log(i);
   }
-  showInfo() {
+  showInfo(i) {
+    this.todayEventList[i].show = !this.todayEventList[i].show;
     this.show = !this.show;
     console.log("bye");
+  }
+  currentWeather(zip: string): void {
+    this.weatherService.getWeatherData(zip).subscribe(response => {
+      this.currentWeatherData = response.weather[0].icon;
+      this.todayReturnDescription = response.weather[0].description;
+      this.todayTemp = this.convertKtoF(response.main.temp);
+    });
   }
   getWeather(zip: string): void {
     // console.log(eventForm.value.date, eventForm.value.time);
@@ -66,7 +88,7 @@ export class TodayComponent implements OnInit {
     });
   }
   convertKtoF(Kalvin: number): number {
-    return (Kalvin - 273.15) * (9 / 5) + 32;
+    return Math.round((Kalvin - 273.15) * (9 / 5) + 32);
   }
   getTraffic(event): void {
     // console.log(eventForm.value.time, eventForm.value.date);
@@ -77,7 +99,12 @@ export class TodayComponent implements OnInit {
       console.log(this.trafficData);
     });
   }
-  markCompleted(): void {
-    this.completed = true;
+  markCompleted(i): void {
+    this.todayTodoList[i].completed = true;
+    this.postCompleted(i);
+  }
+  postCompleted(i): void {
+    this.completedTodo.push(this.todayTodoList[i]);
+    console.log(this.completedTodo);
   }
 }
