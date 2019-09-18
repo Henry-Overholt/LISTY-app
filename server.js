c984e2192810fb4ad18c499ecba1d65c98db0438
Hillary <uptonh13@gmail.com>
2019-09-18 16:05:27 -0400

    Removes special chars from server.js

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-//Install express server
+// Install express server
 const express = require("express");
 const path = require("path");
-​
 const app = express();
-​
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist"));
-​
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
-​
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-
-
-
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Running on port ${process.env.PORT || 8080}`);
+});
